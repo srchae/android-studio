f02bcea5c3007db7ccf8cbacc6b39c89dea074f8
user <surin0636@naver.com>
2021-12-14 00:51:19 +0900

    MyApp_NF

---
+++ app/src/main/java/com/example/a11st_myapplication/MovieList.java
@@ -0,0 +1,5 @@
+package com.example.a11st_myapplication;
+
+public class MovieList {
+    MovieListResult boxOfficeResult;
+}
---
+++ app/src/main/java/com/example/a11st_myapplication/MovieListResult.java
@@ -0,0 +1,12 @@
+package com.example.a11st_myapplication;
+
+
+import java.util.ArrayList;
+
+
+public class MovieListResult {
+    String boxofficeType;
+    String showRange;
+
+    ArrayList<Movie> dailyBoxOfficeList = new ArrayList<Movie>();
+}
